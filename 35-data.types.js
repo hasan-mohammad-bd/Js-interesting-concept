ca09b4c3bc722d69e8b120e0749dffb10855a083
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-25 09:27:09 +0600

    first commit

---
+++ 35-data.types.js
@@ -0,0 +1,13 @@
+//35-1 Module Overview, Dynamic data types
+
+//javascript dynamic type variable
+//===================================================
+//the value determine the type of the variable.
+const a = 19;
+console.log((typeof a));
+const b = "jamal uddian shahen shah"
+console.log(typeof b);
+let c = false;
+console.log(typeof c);
+
+//thats why javascript is called dynamic type of programming language.
